5c1caf0ab1b8d92e779c6970a12549b680ed48f4
Sai <kldtks@live.com>
2018-11-23 18:17:28 +0800

    update wallet token

--- testToken/contracts/TestCoin.sol
+++ testToken/contracts/TestCoin.sol
@@ -1,48 +1,51 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.5.0;
+
+interface ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 _amount, address _token, bytes calldata _data) external;
+}
 
 contract Token {
 
     /// @return total amount of tokens
-    function totalSupply() constant returns (uint256 supply) {}
+    function totalSupply() view public returns (uint256 supply) {}
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function balanceOf(address _owner) public view returns (uint256 balance) {}
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) public returns (bool success) {}
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
 
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) public returns (bool success) {}
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
 }
 
 
 
 contract StandardToken is Token {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
@@ -50,49 +53,58 @@ contract StandardToken is Token {
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
+            emit Transfer(msg.sender, _to, _value);
             return true;
         } else { return false; }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        if (msg.sender == _from){
+            return transfer(_to, _value);
+        }else{
+            //same as above. Replace this line with the following if you want to protect against wrapping uints.
+            //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+            if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+                balances[_to] += _value;
+                balances[_from] -= _value;
+                allowed[_from][msg.sender] -= _value;
+                emit Transfer(_from, _to, _value);
+                return true;
+            } else { return false; }
+        }
+
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
       return allowed[_owner][_spender];
+    }
+
+    function totalSupply() public view returns (uint256){
+      return total;
     }
 
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
+    uint256 total;
 }
 
 
 //name this contract whatever you'd like
-contract TestCoin is StandardToken {
+contract WalletToken is StandardToken {
 
-    function () {
+    function () external {
         //if ether is sent to this address, send it back.
-        throw;
+        revert();
     }
 
     /* Public variables of the token */
@@ -114,23 +126,25 @@ contract TestCoin is StandardToken {
 
 //make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
-    function TestCoin() {
-        balances[msg.sender] = 4*10**27;               // Give the creator all initial tokens 
-        totalSupply = 4*10**27;   // Update total supply 
-        name = "TNT Token";        // Set the name for display purposes
+    constructor() public {
+        balances[msg.sender] = 4*10**27;               // Give the creator all initial tokens
+        total = 4*10**27;   // Update total supply
+        name = "Wallet Token";        // Set the name for display purposes
         decimals = 18;           // Amount of decimals for display purposes
-        symbol = "TNT";         // Set the symbol for display purposes
+        symbol = "WT";         // Set the symbol for display purposes
     }
 
     /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData) public returns (bool success) {
+      if (!approve(_spender, _value)) revert();
 
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
+      ApproveAndCallFallBack(_spender).receiveApproval(
+          msg.sender,
+          _value,
+          address(this),
+          _extraData
+      );
+
+      return true;
     }
 }
